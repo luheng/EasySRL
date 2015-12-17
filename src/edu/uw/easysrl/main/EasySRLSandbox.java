e4fba7d6fd3b5ef0f0cb76dc44eefe10437737a6
luheng <luheng@uw.edu>
2015-12-16 23:10:00 -0800

    toy sentence parsing, fun

--- src/edu/uw/easysrl/main/EasySRLSandbox.java
+++ src/edu/uw/easysrl/main/EasySRLSandbox.java
@@ -5,16 +5,19 @@ import java.io.File;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.text.DecimalFormat;
-import java.util.InputMismatchException;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import edu.stanford.nlp.util.StringUtils;
+import edu.uw.easysrl.dependencies.ResolvedDependency;
+import edu.uw.easysrl.qasrl.qg.QuestionGenerator;
+import edu.uw.easysrl.qasrl.qg.QuestionSlot;
+import edu.uw.easysrl.qasrl.qg.QuestionTemplate;
+import edu.uw.easysrl.qasrl.qg.VerbHelper;
+import edu.uw.easysrl.syntax.grammar.Category;
 import uk.co.flamingpenguin.jewel.cli.ArgumentValidationException;
 import uk.co.flamingpenguin.jewel.cli.CliFactory;
 
@@ -34,16 +37,24 @@ import edu.uw.easysrl.util.Util;
 public class EasySRLSandbox {
 
     public static String[] spielzeuge = {
-          "I saw a squirrel.",
-          "I saw a squirrel with a nut yesterday.",
-          "They increased the rent from $100,000 to $200,000.",
-          "The squirrel increased the price from 1 nut to 2 nuts.",
-          "I want the mug on the table that I usually drink from.",
-          "Could you pass me the mug on the table that I usually drink from?",
-          "Pass me the mug on the table that I usually drink from!"
+            "I saw a squirrel .",
+            "I saw a squirrel with a nut yesterday .",
+            "I saw a squirrel eating a nut yesterday .",
+            "The squirrel refused to leave .",
+            "I promised the squirrel to give it a nut tomorrow .",
+            "They increased the rent from $100,000 to $200,000 .",
+            "The squirrel increased the price from 1 nut to 2 nuts .",
+            "I want the mug on the table that I usually drink from .",
+            "I want the mug on the table that I usually drink water from .",
+            //"Could you pass me the mug on the table that I usually drink from ?",
+            "Pass me the mug on the table that I usually drink from !",
+            "The man with mug was mugged by another man ."
     };
 
+    private static QuestionGenerator questionGenerator;
+
     public static void main(final String[] args) throws IOException, InterruptedException {
+        questionGenerator = new QuestionGenerator();
         try {
             final CommandLineArguments commandLineOptions = CliFactory.parseArguments(CommandLineArguments.class, args);
             final InputFormat input = InputFormat.valueOf(commandLineOptions.getInputFormat().toUpperCase());
@@ -73,18 +84,71 @@ public class EasySRLSandbox {
             System.err.println("===Model loaded: parsing...===");
 
             // Run over toy sentences.
-            int id = 0;
             for (String rawSentence : spielzeuge) {
-                id++;
-                final int id2 = id; // what is this for?
-                final List<CCGandSRLparse> parses = parser.parseTokens(reader.readInput(rawSentence).getInputWords());
-                final String output = printer.printJointParses(parses, id2);
-                System.out.println(output);
+                List<InputReader.InputWord> words = reader.readInput(rawSentence).getInputWords();
+                final List<CCGandSRLparse> parses = parser.parseTokens(words);
+                generateQuestions(words, parses);
             }
 
         } catch (final ArgumentValidationException e) {
             System.err.println(e.getMessage());
             System.err.println(CliFactory.createCli(CommandLineArguments.class).getHelpMessage());
+        }
+    }
+
+    private static void generateQuestions(List<InputReader.InputWord> inputWords, List<CCGandSRLparse> parses) {
+        if (parses == null || parses.size() == 0) {
+            System.out.println("Unable to parse.");
+            return;
+        }
+        List<String> words = new ArrayList<>();
+        List<Category> categories = new ArrayList<>();
+        for (int i = 0; i < inputWords.size(); i++) {
+            words.add(parses.get(0).getLeaf(i).getWord());
+            categories.add(parses.get(0).getLeaf(i).getCategory());
+        }
+
+        words.forEach(w -> System.out.print(w + " ")); System.out.println();
+        categories.forEach(cat -> System.out.print(cat + " ")); System.out.println();
+
+        for (CCGandSRLparse parse : parses) {
+            Collection<ResolvedDependency> dependencies = parse.getDependencyParse();
+            for (ResolvedDependency targetDependency : dependencies) {
+                int predicateIndex = targetDependency.getHead();
+                int argumentNumber = targetDependency.getArgNumber();
+                // Skip copula verb.
+                if (VerbHelper.isCopulaVerb(words.get(predicateIndex))) {
+                    continue;
+                }
+                // Get template.
+                QuestionTemplate template = questionGenerator.getTemplate(predicateIndex, words, categories,
+                        dependencies);
+                if (template == null) {
+                    continue;
+                }
+                // Get question.
+                List<String> question = questionGenerator.generateQuestionFromTemplate(template, argumentNumber);
+                if (question == null) {
+                    continue;
+                }
+                String questionStr = StringUtils.join(question) + " ?";
+
+                // Print sentence and template.
+                String ccgInfo = targetDependency.getCategory() + "_" + targetDependency.getArgNumber();
+                System.out.println(ccgInfo);
+                for (QuestionSlot slot : template.slots) {
+                    String slotStr = (slot.argumentNumber == argumentNumber ?
+                            String.format("{%s}", slot.toString(words)) : slot.toString(words));
+                    System.out.print(slotStr + "\t");
+                }
+                System.out.println();
+                // Print question.
+                System.out.println(questionStr + "\t" + words.get(targetDependency.getArgumentIndex()));
+                // Print target dependency.
+                System.out.println(words.get(targetDependency.getHead()) + ":" + targetDependency.getSemanticRole() +
+                        "\t" + words.get(targetDependency.getArgumentIndex()));
+                System.out.println();
+            }
         }
     }
 }
