767d6c5a7e12b784f4507e453641af360f4b2b98
luheng <luheng@uw.edu>
2016-02-16 15:11:18 -0800

    error analysis wrapup

--- src/edu/uw/easysrl/qasrl/annotation/AlignedAnnotation.java
+++ src/edu/uw/easysrl/qasrl/annotation/AlignedAnnotation.java
@@ -1,5 +1,6 @@
 package edu.uw.easysrl.qasrl.annotation;
 
+import java.io.IOException;
 import java.util.*;
 
 /**
@@ -7,21 +8,25 @@ import java.util.*;
  */
 public class AlignedAnnotation extends RecordedAnnotation {
     Map<String, Integer> annotatorToAnswerId;
+    Map<String, String> annotatorToComment;
     int[] answerDist;
 
     AlignedAnnotation(RecordedAnnotation annotation) {
         super();
         this.iterationId = annotation.iterationId;
         this.sentenceId = annotation.sentenceId;
+        this.sentenceString = annotation.sentenceString;
         this.predicateId = annotation.predicateId;
         this.argumentNumber = annotation.argumentNumber;
         this.predicateCategory = annotation.predicateCategory;
+        this.predicateString = annotation.predicateString;
         this.questionId = annotation.questionId;
         this.question = annotation.question;
         this.answerStrings = annotation.answerStrings;
         this.answerId = annotation.answerId;
         this.goldAnswerId = annotation.goldAnswerId;
         annotatorToAnswerId = new HashMap<>();
+        annotatorToComment = new HashMap<>();
         answerDist = new int[answerStrings.size()];
         Arrays.fill(answerDist, 0);
     }
@@ -31,6 +36,9 @@ public class AlignedAnnotation extends RecordedAnnotation {
         if (this.isSameQuestionAs(annotation) && !annotatorToAnswerId.containsKey(annotator)) {
             annotatorToAnswerId.put(annotator, annotation.answerId);
             answerDist[annotation.answerId] ++;
+            if (annotation.comment != null && !annotation.comment.isEmpty()) {
+                annotatorToComment.put(annotator, annotation.comment);
+            }
             return true;
         }
         return false;
@@ -59,5 +67,47 @@ public class AlignedAnnotation extends RecordedAnnotation {
             });
         }
         return new ArrayList<>(alignedAnnotations.values());
+    }
+
+    @Override
+    public String toString() {
+        // Number of iteration in user session.
+        String result = "ITER=" + iterationId + "\n"
+                + "SID=" + sentenceId + "\t" + sentenceString + "\n"
+                + "PRED=" + predicateId + "\t" + predicateString + "\t" + predicateCategory + "." + argumentNumber + "\n"
+                + "QID=" + questionId + "\t" + question + "\n";
+        for (int i = 0; i < answerStrings.size(); i++) {
+            String match = "";
+            for (int j = 0; j < answerDist[i]; j++) {
+                match += "*";
+            }
+            if (i == goldAnswerId) {
+                match += "G";
+            }
+            result += String.format("%-8s\t%d\t%s\n", match, i, answerStrings.get(i));
+        }
+        for (String annotator : annotatorToComment.keySet()) {
+            result += annotator + ":\t" + annotatorToComment.get(annotator) + "\n";
+        }
+        result += "\n";
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Map<String, List<RecordedAnnotation>> annotations = new HashMap<>();
+        for (String fileName : args) {
+            String[] info = fileName.split("/");
+            String annotator = info[info.length - 1].split("_")[0];
+            System.out.println(annotator);
+            try {
+                annotations.put(annotator, RecordedAnnotation.loadAnnotationRecordsFromFile(fileName));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        List<AlignedAnnotation> alignedAnnotations = AlignedAnnotation.getAlignedAnnotations(annotations, null);
+        alignedAnnotations.stream()
+                .sorted((r1, r2) -> Integer.compare(r1.sentenceId, r2.sentenceId))
+                .forEach(System.out::print);
     }
 }
