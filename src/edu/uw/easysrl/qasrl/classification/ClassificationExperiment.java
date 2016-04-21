fe3203c7c7823a77ac6dd5ab7379492fbaf42f79
luheng <luheng@uw.edu>
2016-04-21 16:01:16 -0700

    xgboost java api

---
+++ src/edu/uw/easysrl/qasrl/classification/ClassificationExperiment.java
@@ -0,0 +1,39 @@
+package edu.uw.easysrl.qasrl.classification;
+
+import com.google.common.collect.ImmutableMap;
+import ml.dmlc.xgboost4j.java.DMatrix;
+import ml.dmlc.xgboost4j.java.Booster;
+import ml.dmlc.xgboost4j.java.XGBoost;
+import ml.dmlc.xgboost4j.java.XGBoostError;
+import scala.tools.cmd.gen.AnyVals;
+
+import java.util.Map;
+
+/**
+ * Trying out XGBoost and stuff ...
+ * Created by luheng on 4/21/16.
+ */
+public class ClassificationExperiment {
+
+    final static String demoPath = "/home/luheng/Tools/xgboost/demo/data/";
+
+    public static void main(final String[] args) throws XGBoostError {
+        System.out.println("how bout this");
+        final DMatrix trainData = new DMatrix(demoPath + "agaricus.txt.train");
+        final DMatrix testData = new DMatrix(demoPath + "agaricus.txt.test");
+        final Map<String, Object> paramsMap = ImmutableMap.of(
+                "eta", 0.1,
+                "max_depth", 2,
+                "objective", "binary:logistic"
+        );
+        final Map<String, DMatrix> watches = ImmutableMap.of(
+                "train", trainData,
+                "test", testData
+        );
+        final int round = 2;
+        Booster booster = XGBoost.train(trainData, paramsMap, round, watches, null, null);
+
+        float[][] predicts = booster.predict(testData);
+        //float[][] leafPredicts = booster.predictLeaf(testData, 0, true);
+    }
+}
---
+++ src/edu/uw/easysrl/qasrl/classification/FeatureExtractor.java
@@ -0,0 +1,7 @@
+package edu.uw.easysrl.qasrl.classification;
+
+/**
+ * Created by luheng on 4/21/16.
+ */
+public class FeatureExtractor {
+}
