784bb6e7038ed3c3f665c89422f5ac992bf3d2e1
luheng <luheng@uw.edu>
2016-04-28 10:33:12 -0700

    ...

--- src/edu/uw/easysrl/qasrl/experiments/ReparsingExperiment.java
+++ src/edu/uw/easysrl/qasrl/experiments/ReparsingExperiment.java
@@ -41,7 +41,7 @@ public class ReparsingExperiment {
           //  "./Crowdflower_data/f891522.csv",                // Round4: jeopardy checkbox, pp only
           //  "./Crowdflower_data/f893900.csv",                   // Round3-pronouns: checkbox, core only, pronouns.
           //  "./Crowdflower_data/f897179.csv"                 // Round2-3: NP clefting questions.
-            "./Crowdflower_data/f902142.csv"
+            "./Crowdflower_data/f902142.csv"                   // Round4: checkbox, pronouns, core only, 300 sentences.
     };
 
     private static QueryPruningParameters queryPruningParameters;
