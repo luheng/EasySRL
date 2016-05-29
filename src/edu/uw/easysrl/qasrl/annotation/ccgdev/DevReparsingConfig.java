83fa704d3c9768f800b6cd12b09a1884d5cae9a2
luheng <luheng@uw.edu>
2016-05-29 14:26:50 -0700

    local chagnes

--- src/edu/uw/easysrl/qasrl/annotation/ccgdev/DevReparsingConfig.java
+++ src/edu/uw/easysrl/qasrl/annotation/ccgdev/DevReparsingConfig.java
@@ -4,4 +4,20 @@ package edu.uw.easysrl.qasrl.annotation.ccgdev;
  * Created by luheng on 5/29/16.
  */
 public class DevReparsingConfig {
+    boolean fixPronouns = false;
+    boolean fixSubspans = false;
+    boolean fixAppositves = false;
+    boolean fixRelatives = false;
+    boolean fixConjunctions = false;
+    boolean useSubspanDisjunctives = false;
+    boolean useOldConstraints = false;
+    int positiveConstraintMinAgreement = 3;
+    int negativeConstraintMaxAgreement = 2;
+    double positiveConstraintPenalty = 1.0;
+    double negativeConstraintPenalty = 1.0;
+    double supertagPenalty = 1.0;
+
+    public DevReparsingConfig() {
+
+    }
 }
