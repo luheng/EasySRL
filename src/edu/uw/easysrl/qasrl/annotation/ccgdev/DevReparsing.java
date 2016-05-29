dd5ab0fe1eb4488af0fe6c6f2929a0fe520e2878
luheng <luheng@uw.edu>
2016-05-28 23:58:08 -0700

    ...

--- src/edu/uw/easysrl/qasrl/annotation/ccgdev/DevReparsing.java
+++ src/edu/uw/easysrl/qasrl/annotation/ccgdev/DevReparsing.java
@@ -28,8 +28,8 @@ import java.util.stream.Stream;
 public class DevReparsing {
 
     ///////////////////////////////// Knobs...
-    final static boolean fixPronouns = false;
-    final static boolean fixSubspans = false;
+    final static boolean fixPronouns = true;
+    final static boolean fixSubspans = true;
     final static boolean fixAppositves = true;
     final static boolean fixRelatives = true;
     final static boolean useSubspanDisjunctives = false;
